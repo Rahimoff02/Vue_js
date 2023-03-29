669dc2ebc83eac07a45af334e8284917640b9bee
Rahimoff <rehimov.faiq15@gmail.com>
2023-03-29 17:04:19 +0400

    header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import Footer from './views/layouts/footer.vue'
+import Header from './views/layouts/header.vue'
+
 </script>
 
 <template>
+
+<Header/>
 
   <RouterView />
 
